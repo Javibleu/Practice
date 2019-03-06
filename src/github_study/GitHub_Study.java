fe28d14d21b440139c8a8b8baa239d4293307a67
Javier <nievasj@sheridancollege.ca>
2019-03-06 17:10:52 -0500

    Initial Commit Step2 Apunte

---
+++ src/github_study/GitHub_Study.java
@@ -0,0 +1,28 @@
+ /* 
+Name:       Javier Nievas 
+Assignment: Assignment(******************)
+Program:    PROG24178 
+Date:       6-Mar-2019
+ 
+Description: [program description in your own words] 
+*/
+
+
+
+package github_study;
+
+/**
+ *
+ * @author Javier Nievas
+ */
+public class GitHub_Study {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+
+    }
+
+}
